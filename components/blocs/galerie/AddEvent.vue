5979f4c7d6edb3839946605b8fbc89c4bce264d3
Gaëtan Perrier <gaetan.perrier@u-picardie.fr>
2023-12-07 13:40:21 +0100

    ajout dynamique theme

--- components/blocs/galerie/AddEvent.vue
+++ components/blocs/galerie/AddEvent.vue
@@ -15,7 +15,9 @@
                 <input type="text" id="Lieu_Eve" name="Lieu_Eve" required>
 
                 <label for="">Themes</label>
-                <select name="ID_Theme" id="ID_Theme"></select>
+                <select name="ID_Theme" id="ID_Theme">
+                    <option v-for="item in dataTheme" :key="item.ID_Theme" :value="item.ID_Theme">{{ item.Nom_Theme }}</option>
+                </select>
 
                 <label for="Photo">Image</label>
                 <input type="file" id="Photo" name="Photo"  required>
@@ -23,15 +25,35 @@
                 <input type="submit" name="submit-button" value="Envoyer">
             </div>
         </section>
-
-
-
-        <!--
-            
-        -->
     </form>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            dataTheme: [],
+        }
+    },
+    mounted() {
+        this.fetchTheme()
+    },
+
+    methods: {
+        async fetchTheme(){
+            const url = "https://cidapi.alwaysdata.net/get_themes/";
+            try {
+                const res = await fetch(url);
+                this.dataTheme = await res.json();
+                return {dataTheme};
+            }catch (error){
+                console.error('Erreur de récupération des données :' ,error);
+                return {dataTheme:[]};
+            }
+        }
+    },
+};
+</script>
 
 <style>
 .image-input{
--- components/blocs/galerie/AddImg.vue
+++ components/blocs/galerie/AddImg.vue
@@ -7,30 +7,72 @@
                     <input type="file" id="Photo" name="Photo"  required>
                 </div>
  
-                <div class="input-container u-flex u-flex-direction-column u-justify-content-center w50">
+                <div class="input-container u-flex u-flex-direction-column u-justify-content-evenly w50">
+                    <div>
+                        <label for="Titre">Nom de l'image</label>
+                        <input type="text" id="Titre" name="Titre" required>
+                    </div>
 
-                    <h2 class="w100 fs25px u-mb20">Ajouter une image</h2>
-                    
-                    <label for="Titre">Titre</label>
-                    <input type="text" id="Titre" name="Titre" required>
+                    <div>
+                        <label for="Personnes">Personnes sur l'image (facultatif)</label>
+                        <input type="text" id="Personnes" name="Personnes">
+                    </div>
 
-                    <label for="Personnes">personnes</label>
-                    <input type="text" id="Personnes" name="Personnes">
+                    <div>
+                        <label for="Photographe">Photographe</label>
+                        <input type="text" id="Photographe" name="Photographe">
+                    </div>
 
-                    <label for="Photographe">Photographe</label>
-                    <input type="text" id="Photographe" name="Photographe">
+                    <div>
+                        <label for="Lieu_photo">Lieu</label>
+                        <input type="text" id="Lieu_Photo" name="Lieu_Photo">
+                    </div>
 
-                    <label for="Lieu_photo">Lieu</label>
-                    <input type="text" id="Lieu_Photo" name="Lieu_Photo">
+                    <div>
+                        <label for="Date_Photo">Date</label>
+                        <input type="date" id="Date_Photo" name="Date_Photo" required>
+                    </div>
 
-                    <label for="Date_Photo">Date</label>
-                    <input type="date" id="Date_Photo" name="Date_Photo" required>
+                    <div>
+                        <label for="ID_Theme">Theme</label>
+                        <select name="ID_Theme" id="ID_Theme">
+                            <option v-for="item in dataTheme" :key="item.ID_Theme" :value="item.ID_Theme">{{ item.Nom_Theme }}</option>
+                        </select>
+                    </div>
                 </div>
             </div>
-            <input type="submit" name="submit-button" value="Envoyer">
+            <input class="bouton-publier" type="submit" name="submit-button" value="Publier">
         </section>
     </form>
 </template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            dataTheme: [],
+        }
+    },
+    mounted() {
+        this.fetchTheme()
+    },
+
+    methods: {
+        async fetchTheme(){
+            const url = "https://cidapi.alwaysdata.net/get_themes/";
+            try {
+                const res = await fetch(url);
+                this.dataTheme = await res.json();
+                return {dataTheme};
+            }catch (error){
+                console.error('Erreur de récupération des données :' ,error);
+                return {dataTheme:[]};
+            }
+        }
+    },
+};
+</script>
 
 
 <style>
@@ -49,11 +91,22 @@
     input{
         width: 100%;
         height: 50px;
-        margin-bottom: 30px;
         font-size: 15px;
+        border: 1px solid gray;
+        border-radius: 3px;
+        margin-top: 10px;
     }
-    label{
-        margin-bottom: 10px;
+    select{
+        width: 100%;
+        height: 50px;
+        font-size: 15px;
+        background-color: white;
+        border: 1px solid gray;
+        border-radius: 3px;
+        margin-top: 10px;
+    }
+    .bouton-publier{
+        background-color: blue;
     }
 }
    
